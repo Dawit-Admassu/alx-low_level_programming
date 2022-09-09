462f1c92e41b100b062774fdf05a45d037585dc2
Dawit.Admassu <daveadm1985@gmail.com>
2022-09-09 12:07:51 -0700

    error

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ int main(void)
 	while (e < 58)
 	{
 		i = 48;
-   		while (i < 58)
+		while (i < 58)
 		{
 			g = 48;
 			while (g < 58)
@@ -28,8 +28,8 @@ int main(void)
 					putchar(g);
 					if (i == 56 && e == 55 && g == 57)
 					{
-						break;	
-					}	
+						break;
+					}
 					putchar(',');
 					putchar(' ');
 				}
