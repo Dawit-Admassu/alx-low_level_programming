bd21fff9d9bce88ab618c621629772c516e5efcc
Dawit.Admassu <daveadm1985@gmail.com>
2022-09-13 06:35:36 -0700

    error

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{
+	    /*storing the charachter to be written to stdout*/
+	    char str[10] = "_putchar";
+            /*transfers data to stdout using the putchar() function*/
+	   putchar(str);
+	   return 0;
+}
