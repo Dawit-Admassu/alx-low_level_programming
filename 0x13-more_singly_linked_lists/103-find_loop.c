a486173565f5fa08cf93ca8ce7e8c52ec0bd8631
Dawit.Admassu <daveadm1985@gmail.com>
2022-10-26 11:49:47 -0700

    103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -30,7 +30,7 @@ listint_t *find_listint_loop(listint_t *head)
 					p2 = p2->next;
 				}
 				if (p2->next == head)
-					break
+					break;
 																			head = head->next;
 																		}
 			return (p2->next);
